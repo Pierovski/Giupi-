35c957adb38da0250f1020ee1378b0333b406476
Pierovski <pieroczavaleta96@gmail.com>
2026-07-11 16:05:56 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - El motor de Giupi (Actualizado para Auto-Updates y Galería)
-const CACHE_NAME = 'giupi-v4.1'; // Incrementa este número cuando lances grandes cambios
+const CACHE_NAME = 'giupi-v4.2'; // Incrementa este número cuando lances grandes cambios
 
 const assets = [
   '/',
